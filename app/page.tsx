ccc56d9385cb68e7931182ab7a7c1af26097c463
Kevin Neaton <kevin@neaton.xyz>
2023-03-22 01:44:03 -0400

    add MintButton to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import MintButton from '@/components/pdk/MintButton'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col gap-[64px] items-center p-24">
       <h1>PDK-001</h1>
+      <MintButton />
+      <MintButton disabled />
     </main>
   )
 }

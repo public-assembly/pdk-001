6f8d4de91c755c2a0a3da88ef74ee9739d318ee7
Kevin Neaton <kevin@neaton.xyz>
2023-03-22 01:24:11 -0400

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000000',
+      white: '#FFFFFF',
+      gray: {
+        100: '#F9F1FA',
+        500: '#948696',
+      },
+      pink: {
+        400: '#E883EE',
+        500: '#E62DF2',
+      }
+    }
+  }
 }

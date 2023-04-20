1680af827ec57249afc267b61dbec0b307ead674
Kevin Neaton <kevin@neaton.xyz>
2023-04-19 23:56:15 -0400

    improved button implementation
    - Use border-image to simplify css and reduce asset count.
    - Split text images into words to enable text wrapping.
      rounding errors, at least in brave).
    - Disable selection.

--- components/pdk/Button/index.tsx
+++ components/pdk/Button/index.tsx
@@ -1,14 +1,13 @@
 import styles from './styles.module.css'
-
 import clsx from 'clsx'
 
 export type ButtonProps = React.ComponentPropsWithoutRef<"button">
 
-export function Button({className, children, ...props}: ButtonProps) {
+export default function Button({className, children, ...props}: ButtonProps) {
   return (
-    <button className={clsx(styles.btn, className)} {...props}>
+    <button className={clsx(styles.btn, className, "select-none")} {...props}>
       {/* div used to shift contents when clicked */}
-      <div>{children}</div>
+      <div className={styles.btnBody}>{children}</div>
     </button>
   )
 } 
--- components/pdk/MintButton.tsx
+++ components/pdk/MintButton.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
-import { Button } from './Button'
+import Button from './Button'
 
-import iconDevil from './assets/icon-devil.png'
-import textMintMyDestruction from './assets/text-mint-my-destruction.png'
+import iconDevil from './assets/icon/devil.png'
+import iconDevilDisabled from './assets/icon/devil_disabled.png'
 
-import iconDevilDisabled from './assets/icon-devil_disabled.png'
-import textMintMyDestructionDisabled from './assets/text-mint-my-destruction_disabled.png'
+import textMint from './assets/text/mint.png'
+import textMy from './assets/text/my.png'
+import textDestruction from './assets/text/destruction.png'
 
+import textMintDisabled from './assets/text/mint_disabled.png'
+import textMyDisabled from './assets/text/my_disabled.png'
+import textDestructionDisabled from './assets/text/destruction_disabled.png'
 
 export type MintButtonProps = React.ComponentPropsWithoutRef<"button">
 
@@ -14,7 +18,21 @@ export default function MintButton({disabled, ...props}: MintButtonProps) {
   return (
     <Button className="px-[62.5px] py-[25px]" {...props} disabled={disabled}>
       <Image className="m-auto mb-[7px]" src={disabled ? iconDevilDisabled : iconDevil} alt="Devil icon" />
-      <Image className="m-auto" src={disabled ? textMintMyDestructionDisabled : textMintMyDestruction} alt="Mint My Destruction" />
+      <div className="flex flex-wrap gap-[10px] justify-center ">
+        {disabled ? (
+          <>
+            <Image className="max-w-none" src={textMintDisabled} alt="Mint" />
+            <Image className="max-w-none" src={textMyDisabled} alt="My" />
+            <Image className="max-w-none" src={textDestructionDisabled} alt="Destruction" />
+          </>
+        ) : (
+          <>
+            <Image className="max-w-none" src={textMint} alt="Mint" />
+            <Image className="max-w-none" src={textMy} alt="My" />
+            <Image className="max-w-none" src={textDestruction} alt="Destruction" />
+          </>
+        )}
+      </div>
     </Button>
   )
 }

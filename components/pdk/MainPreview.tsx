01a971810df90309a0ae6f5e60ecab0f32e6b65c
Kevin Neaton <kevin@neaton.xyz>
2023-06-07 23:57:33 -0400

    add Preview components

---
+++ components/pdk/MainPreview.tsx
@@ -0,0 +1,9 @@
+import Border from '@/components/pdk/Border'
+
+export default function MainPreview() {
+  return (
+    <div className="relative w-[668px] h-[668px] w-max-ful h-max-full">
+      <Border className="absolute top-0 right-0 bottom-0 left-0" />
+    </div>
+  )
+}
---
+++ components/pdk/PreviewOption.tsx
@@ -0,0 +1,10 @@
+import Border from '@/components/pdk/Border'
+
+export default function PreviewOption() {
+  return (
+    <div className="relative w-[208px] h-[208px] w-max-ful h-max-full">
+      <Border className="absolute top-0 right-0 bottom-0 left-0" />
+    </div>
+  )
+}
+
